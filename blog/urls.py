e6b967515ac5196dca785c075d22df6a21c63619
menadeimer@gmail.com <menadeimer@github.com>
2018-01-25 17:04:50 -0500

    Se ha agregado la opción de guardar y editar post del blog, además se la ha añadido estilo a las entradas y tiene opción de redirigir una entrada a una url especifica

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
-from django.urls import include, path
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
-	path('', views.post_list),
+	re_path(r'^$', views.post_list),
+	re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	re_path(r'^post/new/$', views.post_new, name='post_new'),
+	re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
